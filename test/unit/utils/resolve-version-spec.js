c1ca6c0240cb4ed3caa14d688a0bce69fc07965a
Austin Burdine <acburdine@gmail.com>
2016-10-03 13:28:04 -0500

    :art: rename version to resolve-version to better reflect what it does

--- test/unit/utils/resolve-version-spec.js
+++ test/unit/utils/resolve-version-spec.js
@@ -3,11 +3,11 @@ var expect = require('chai').expect,
     rewire = require('rewire'),
     Promise = require('bluebird'),
 
-    findVersion = rewire('../../../lib/utils/version');
+    resolveVersion = rewire('../../../lib/utils/resolve-version');
 
-describe('Unit: findVersion', function () {
+describe('Unit: resolveVersion', function () {
     it('rejects for versions less than 1.0.0-alpha.1', function () {
-        return findVersion('0.11.0').then(function () {
+        return resolveVersion('0.11.0').then(function () {
             throw new Error('Version finder should not have resolved.');
         }).catch(function (error) {
             expect(error).to.equal('Ghost-CLI cannot install versions of Ghost less than 1.0.0');
@@ -15,11 +15,11 @@ describe('Unit: findVersion', function () {
     });
 
     it('rejects if result from npm is invalid JSON', function () {
-        var reset = findVersion.__set__('npm', function () {
+        var reset = resolveVersion.__set__('npm', function () {
             return Promise.resolve('invalid json');
         });
 
-        return findVersion().then(function () {
+        return resolveVersion().then(function () {
             throw new Error('Version finder should not have resolved.');
         }).catch(function (error) {
             expect(error).to.equal('Ghost-CLI was unable to load versions from NPM.');
@@ -28,11 +28,11 @@ describe('Unit: findVersion', function () {
     });
 
     it('rejects if no versions are found', function () {
-        var reset = findVersion.__set__('npm', function () {
+        var reset = resolveVersion.__set__('npm', function () {
             return Promise.resolve('[]');
         });
 
-        return findVersion().then(function () {
+        return resolveVersion().then(function () {
             throw new Error('Version finder should not have resolved');
         }).catch(function (error) {
             expect(error).to.equal('No valid versions found.');
@@ -42,11 +42,11 @@ describe('Unit: findVersion', function () {
 
     describe('without existing version', function () {
         it('correctly filters out versions less than 1.0.0-alpha.1', function () {
-            var reset = findVersion.__set__('npm', function () {
+            var reset = resolveVersion.__set__('npm', function () {
                 return Promise.resolve('["0.10.1", "0.11.0"]');
             });
 
-            return findVersion().then(function () {
+            return resolveVersion().then(function () {
                 throw new Error('Version finder should not have resolved');
             }).catch(function (error) {
                 expect(error).to.equal('No valid versions found.');
@@ -55,11 +55,11 @@ describe('Unit: findVersion', function () {
         });
 
         it('errors if specified version is not in list of valid versions', function () {
-            var reset = findVersion.__set__('npm', function () {
+            var reset = resolveVersion.__set__('npm', function () {
                 return Promise.resolve('["1.0.0", "1.0.1"]');
             });
 
-            return findVersion('1.0.5').then(function () {
+            return resolveVersion('1.0.5').then(function () {
                 throw new Error('Version finder should not have resolved');
             }).catch(function (error) {
                 expect(error).to.equal('Invalid version specified.');
@@ -68,22 +68,22 @@ describe('Unit: findVersion', function () {
         });
 
         it('resolves with specified version if it exists and is valid', function () {
-            var reset = findVersion.__set__('npm', function () {
+            var reset = resolveVersion.__set__('npm', function () {
                 return Promise.resolve('["1.0.0", "1.0.1"]');
             });
 
-            return findVersion('1.0.0').then(function (version) {
+            return resolveVersion('1.0.0').then(function (version) {
                 expect(version).to.equal('1.0.0');
                 reset();
             });
         });
 
         it('resolves with latest version if no version specified', function () {
-            var reset = findVersion.__set__('npm', function () {
+            var reset = resolveVersion.__set__('npm', function () {
                 return Promise.resolve('["1.0.0", "1.0.1"]');
             });
 
-            return findVersion().then(function (version) {
+            return resolveVersion().then(function (version) {
                 expect(version).to.equal('1.0.1');
                 reset();
             });
@@ -92,11 +92,11 @@ describe('Unit: findVersion', function () {
 
     describe('with existing version', function () {
         it('correctly filters out all versions greater than the specified one', function () {
-            var reset = findVersion.__set__('npm', function () {
+            var reset = resolveVersion.__set__('npm', function () {
                 return Promise.resolve('["1.0.0", "1.0.1"]');
             });
 
-            return findVersion(null, '1.0.1').then(function () {
+            return resolveVersion(null, '1.0.1').then(function () {
                 throw new Error('Version finder should not have resolved');
             }).catch(function (error) {
                 expect(error).to.equal('No valid versions found.');
