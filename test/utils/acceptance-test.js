81b1564e323e2b7f522c78432b1e9d5258687e8a
Austin Burdine <acburdine@gmail.com>
2017-03-04 03:10:13 -0600

    🎨 acceptance test improvements
    no issue
    - refactor acceptance test helpers to improve further usage
    - deps: tmp@0.0.31

---
+++ test/utils/acceptance-test.js
@@ -0,0 +1,219 @@
+// Borrowed idea from denali's (denali-js/denali) acceptance test helpers
+// License re-printed here:
+/*
+    Copyright 2016 Dave Wasmer
+
+    Permission is hereby granted, free of charge, to any person obtaining
+    a copy of this software and associated documentation files (the
+    "Software"), to deal in the Software without restriction, including
+    without limitation the rights to use, copy, modify, merge, publish,
+    distribute, sublicense, and/or sell copies of the Software, and to
+    permit persons to whom the Software is furnished to do so, subject to
+    the following conditions:
+
+    The above copyright notice and this permission notice shall be
+    included in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+    LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+    OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+    WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+'use strict';
+const fs = require('fs-extra');
+const cp = require('child_process');
+const tmp = require('tmp');
+const find = require('lodash/find');
+const each = require('lodash/each');
+const path = require('path');
+const env = require('./env');
+
+module.exports = class AcceptanceTest {
+    constructor(command, options) {
+        options = options || {};
+
+        this.command = command;
+        this.dir = options.dir || tmp.dirSync({unsafeCleanup: true}).name;
+        this.cliPath = path.join(process.cwd(), 'bin', 'ghost');
+    }
+
+    setup(type) {
+        if (type && env[type]) {
+            if (env[type].dirs) {
+                // create directories first
+                each(env[type].dirs, (dir) => {
+                    fs.ensureDirSync(path.join(this.dir, dir));
+                });
+            }
+
+            if (env[type].links) {
+                // create symlinks
+                each(env[type].links, (link) => {
+                    fs.ensureSymlinkSync(path.join(this.dir, link[0]), path.join(this.dir, link[1]));
+                });
+            }
+
+            if (env[type].files) {
+                // then create files
+                each(env[type].files, (file) => {
+                    fs[(file.json ? 'writeJsonSync' : 'writeFileSync')](path.join(this.dir, file.path), file.content);
+                });
+            }
+        }
+    }
+
+    path(file) {
+        return path.join(this.dir, file);
+    }
+
+    /**
+     * Runs the particular AcceptanceTest command without parsing contents
+     * Returns a promise that resolves when the command succeeds, or rejects if
+     * a) the command fails, or b) options.failOnStdErr is set to true and output
+     * is rendered to stdErr
+     *
+     * @param {Object} options options
+     *                         - failOnStdErr: fail if stderr is written to
+     *                         - env: environment variables to pass to the process
+     *
+     * @return {Promise} Promise that resolves if the command succeeds, otherwise rejects
+     */
+    run(options) {
+        options = options || {};
+
+        return new Promise((resolve, reject) => {
+            cp.exec(`${this.cliPath} ${this.command}`, {
+                env: Object.assign({}, process.env, options.env || {}),
+                cwd: this.dir
+            }, (err, stdout, stderr) => {
+                if (err || (options.failOnStdErr && stderr.length > 0)) {
+                    err = err || new Error('\n');
+                    err.message += `'ghost ${this.command}' failed\ncwd: ${this.cwd}\n` +
+                        `stdout:\n ${stdout} \nstderr: ${stderr}`;
+                    reject(err);
+                } else {
+                    resolve({
+                        stdout: stdout,
+                        stderr: stderr,
+                        dir: this.dir
+                    });
+                }
+            });
+        });
+    }
+
+    /**
+     * Invoke the command and poll the output every options.pollInterval. Useful for commands that
+     * have a definitely completion (i.e. 'build', not 'serve'). Each poll of the output will run the
+     * supplied options.checkOutput method, passing in the stdout and stderr buffers. If the
+     * options.checkOutput method returns a truthy value, the returned promise will resolve.
+     * Otherwise, it will continue to poll the output until options.timeout elapses, after which the
+     * returned promsie will reject.
+     *
+     * @param {Boolean} options.failOnStderr Should any output to stderr result in a rejected promise?
+     * @param {Function} options.checkOutput A function invoked with the stdout and stderr buffers of the invoked
+     *                            command, and should return true if the output passes
+     *
+     * @return {Promise} Promise for the command
+     */
+    spawn(options) {
+        options = options || {};
+
+        return new Promise((resolve, reject) => {
+            this.spawnedCommand = cp.spawn(this.cliPath, this.command.split(' '), {
+                env: Object.assign({}, process.env, {
+                    NODE_ENV: this.environment
+                }, options.env || {}),
+                cwd: this.dir,
+                stdio: 'pipe'
+            });
+
+            // Cleanup spawned processes if our process is killed
+            let cleanup = this.cleanup.bind(this);
+            process.on('exit', cleanup.bind(this));
+
+            // Buffer up the output so the polling timer can check it
+            let stdoutBuffer = '';
+            let stderrBuffer = '';
+            this.spawnedCommand.stdout.on('data', (d) => {
+                let chunk = d.toString();
+
+                if (options.stdin) {
+                    let stdin = find(options.stdin, (obj) => obj.when.test(chunk));
+
+                    if (stdin) {
+                        this.spawnedCommand.stdin.write(`${stdin.write}\n`);
+                    }
+                }
+
+                stdoutBuffer += chunk;
+            });
+            this.spawnedCommand.stderr.on('data', (d) => {
+                stderrBuffer += d.toString();
+            });
+
+            // Handle errors from the child process
+            this.spawnedCommand.stdout.on('error', reject);
+            this.spawnedCommand.stderr.on('error', reject);
+            this.spawnedCommand.on('error', reject);
+            this.spawnedCommand.on('exit', () => {
+                process.removeListener('exit', cleanup);
+                this.cleanup();
+
+                if (options.rejectOnExit) {
+                    let message = `Command 'ghost ${this.command}' exited before matching the checkOutput function.\n` +
+                        `stdout: ${stdoutBuffer} \nstderr: ${stderrBuffer}`;
+                    reject(new Error(message));
+                    return;
+                }
+
+                resolve({
+                    stdout: stdoutBuffer,
+                    stderr: stderrBuffer,
+                    dir: this.dir
+                });
+            });
+
+            // Poll periodically to check the results
+            this.pollOutput = setInterval(() => {
+                if (stderrBuffer.length > 0 && options.failOnStderr) {
+                    process.removeListener('exit', cleanup);
+                    this.cleanup();
+                    let error = new Error(`'${this.command}' printed to stderr with failOnStderr enabled:\n`);
+                    error.message += `cwd: ${this.dir} \nstdout: ${stdoutBuffer} \nstderr: ${stderrBuffer};`
+                    reject(error);
+                }
+                let passed = options.checkOutput(stdoutBuffer, stderrBuffer, this.dir);
+                if (passed) {
+                    process.removeListener('exit', cleanup);
+                    this.cleanup();
+                    resolve();
+                }
+            }, options.pollInterval || 50);
+
+            // Ensure the test fails if we don't pass the test after a while
+            let timeout = options.timeout || (process.env.CI ? 5 * 60000 : 3 * 60000);
+
+            this.fallbackTimeout = setTimeout(() => {
+                process.removeListener('exit', cleanup);
+                this.cleanup();
+                let message = `Timeout of ${(timeout / 1000) / 60} minutes exceeded for spawned command: ${this.command}\n`;
+
+                message += `stdout: ${stdoutBuffer} \nstderr: ${stderrBuffer}`
+                reject(new Error(message));
+            }, timeout);
+        });
+    }
+
+    /**
+     * Internal cleanup method to clean up timers and processes.
+     */
+    cleanup() {
+        this.spawnedCommand.kill();
+        clearInterval(this.pollOutput);
+        clearTimeout(this.fallbackTimeout);
+    }
+}
