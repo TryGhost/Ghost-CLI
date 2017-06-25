793715bb2a936a24f691f4f3d0d0d3754e5b2de8
Austin Burdine <acburdine@gmail.com>
2017-06-24 22:31:22 -0400

    feat(cli): swap out commander for yargs parsing
    closes #179
    - make Command class an actual class
    - refactor bootstrap & clean up command invocation stack

--- test/utils/env.js
+++ test/utils/env.js
@@ -1,4 +1,10 @@
-module.exports = {
+'use strict';
+const fs = require('fs-extra');
+const tmp = require('tmp');
+const path = require('path');
+const isObject = require('lodash/isObject');
+
+const builtin = {
     full: {
         dirs: ['versions/1.0.0', 'config', 'content'],
         links: [
@@ -26,5 +32,40 @@ module.exports = {
                 json: true
             }
         ]
+    }
+};
+
+module.exports = function setupEnv(typeOrDefinition, dir) {
+    let setup = isObject(typeOrDefinition) ? typeOrDefinition : builtin[typeOrDefinition];
+
+    if (!setup) {
+        return null;
+    }
+
+    dir = dir || tmp.dirSync({unsafeCleanup: true}).name;
+
+    if (setup.dirs) {
+        setup.dirs.forEach((dirToCreate) => {
+            fs.ensureDirSync(path.join(dir, dirToCreate));
+        });
+    }
+
+    if (setup.links) {
+        setup.links.forEach((link) => {
+            fs.ensureSymlinkSync(path.join(dir, link[0]), path.join(dir, link[1]));
+        });
+    }
+
+    if (setup.files) {
+        setup.files.forEach((file) => {
+            fs[(file.json ? 'writeJsonSync' : 'writeFileSync')](path.join(dir, file.path), file.content);
+        });
+    }
+
+    return {
+        dir: dir,
+        cleanup: () => {
+            fs.removeSync(dir);
+        }
     }
 };
