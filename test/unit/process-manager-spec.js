1ef28bf862e57cf277dabab2bbe4e920d717d04f
Austin Burdine <acburdine@gmail.com>
2017-07-02 22:24:47 -0400

    feat(process): add enable/disable support
    closes #248
    - add isEnabled, enable, disable methods to process manager
    - add support for enable/disable to start and stop commands

--- test/unit/process-manager-spec.js
+++ test/unit/process-manager-spec.js
@@ -29,6 +29,23 @@ describe('Unit: Process Manager', function () {
         });
     });
 
+    describe('supportsEnableBehavior', function () {
+        it('returns false if the class does not implement all of the methods for enable behavior', function () {
+            let instance = new ProcessManager({}, {}, {});
+            expect(ProcessManager.supportsEnableBehavior(instance)).to.be.false;
+        });
+
+        it('returns true if all the methods for enable behavior are implemented', function () {
+            class TestProcess extends ProcessManager {
+                isEnabled() { }
+                enable() { }
+                disable() { }
+            }
+            let instance = new TestProcess({}, {}, {});
+            expect(ProcessManager.supportsEnableBehavior(instance)).to.be.true;
+        });
+    });
+
     it('restart base implementation calls start and stop methods', function () {
         let instance = new ProcessManager({}, {}, {});
         let startStub = sinon.stub(instance, 'start').resolves();
@@ -39,5 +56,35 @@ describe('Unit: Process Manager', function () {
             expect(startStub.calledOnce).to.be.true;
             expect(stopStub.calledBefore(startStub)).to.be.true;
         })
+    });
+
+    it('base start implementation returns a promise', function () {
+        let instance = new ProcessManager({}, {}, {});
+        let start = instance.start();
+
+        expect(start).to.be.an.instanceof(Promise);
+        return start;
+    });
+
+    it('base stop implementation returns a promise', function () {
+        let instance = new ProcessManager({}, {}, {});
+        let stop = instance.stop();
+
+        expect(stop).to.be.an.instanceof(Promise);
+        return stop;
+    });
+
+    it('base willRun method returns true', function () {
+        expect(ProcessManager.willRun()).to.be.true;
+    });
+
+    it('base error method re-throws the error', function () {
+        let instance = new ProcessManager({}, {}, {});
+        try {
+            instance.error({error: true});
+            expect(false, 'error should have been thrown').to.be.true;
+        } catch (e) {
+            expect(e).to.deep.equal({error: true});
+        }
     });
 });
