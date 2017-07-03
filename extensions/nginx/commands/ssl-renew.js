946f6dde3da97d3229ee5f6960594f46f4185e10
Austin Burdine <acburdine@gmail.com>
2017-07-03 00:35:39 -0400

    feat(nginx): move letsencrypt to its own file, add ssl renew command
    refs #190
    - adds ssl-renew command

---
+++ extensions/nginx/commands/ssl-renew.js
@@ -0,0 +1,22 @@
+'use strict';
+const cli = require('../../../lib');
+const letsencrypt = require('../letsencrypt');
+
+class SslRenewCommand extends cli.Command {
+    run() {
+        let instance = this.system.getInstance();
+
+        if (!instance.cliConfig.has('extension.sslemail')) {
+            return Promise.reject(new cli.errors.SystemError('No saved email found, skipping automatic letsencrypt renewal'));
+        }
+
+        let email = instance.cliConfig.get('extension.sslemail');
+        return this.ui.run(letsencrypt(instance, email, false), 'Renewing SSL certificate')
+            .catch((error) => Promise.reject(new cli.errors.ProcessError(error)));
+    }
+}
+
+SslRenewCommand.description = 'Renew an SSL certificate for a Ghost installation';
+
+module.exports = SslRenewCommand;
+
