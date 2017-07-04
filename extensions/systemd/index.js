5f3b0b849c78f52b6f57eef05f5a3dfbe1a6512b
Austin Burdine <acburdine@gmail.com>
2017-07-04 17:48:03 -0400

    feat(linux): add linux user extension
    closes #189
    - add linux extension that creates a `ghost` user on the system, used by systemd (and potentially any other process manager)

--- extensions/systemd/index.js
+++ extensions/systemd/index.js
@@ -2,6 +2,7 @@
 
 const fs = require('fs-extra');
 const path = require('path');
+const execa = require('execa');
 const template = require('lodash/template');
 
 const cli = require('../../lib');
@@ -11,18 +12,31 @@ class SystemdExtension extends cli.Extension {
         let instance = this.system.getInstance();
 
         if (!argv.local && instance.config.get('process') === 'systemd') {
-            cmd.addStage('systemd', this._setup.bind(this));
+            cmd.addStage('systemd', this._setup.bind(this), 'linux-user');
         }
     }
 
-    _setup(argv, ctx) {
+    _setup(argv, ctx, task) {
+        let uid;
+
+        try {
+            uid = execa.shellSync('id -u ghost').stdout;
+        } catch (e) {
+            if (!e.message.match(/no such user/)) {
+                return Promise.reject(e);
+            }
+
+            this.ui.log('Ghost user has not been set up, please run `ghost setup linux-user` first', 'yellow');
+            return task.skip();
+        }
+
         let service = template(fs.readFileSync(path.join(__dirname, 'ghost.service.template'), 'utf8'));
         let serviceFilename = `ghost_${ctx.instance.name}.service`;
 
         return ctx.instance.template(service({
             name: ctx.instance.name,
             dir: process.cwd(),
-            user: process.getuid(),
+            user: uid,
             environment: this.system.environment,
             ghost_exec_path: process.argv.slice(0,2).join(' ')
         }), 'systemd service file', serviceFilename, '/lib/systemd/system').then(() => {
