9bf39ce62c84dba8bd95fd9a0ab281016d2c7f99
kirrg001 <katharina.irrgang@googlemail.com>
2017-07-05 10:16:28 +0000

    fix incorrect syntax

--- extensions/mysql/index.js
+++ extensions/mysql/index.js
@@ -26,7 +26,7 @@ class MySQLExtension extends cli.Extension {
         return this.canConnect()
             .then(() => {
                 if (this.databaseConfig.connection.user === 'root') {
-                    return this.ui.confirm('Your MySQL user is root. Would you like to create a `ghost` MySQL user?', true)
+                    return this.ui.confirm('Your MySQL user is root. Would you like to create a custom Ghost MySQL user?', true)
                         .then((res) => {
                             if (res.yes) {
                                 return this.createMySQLUser(ctx);
@@ -50,13 +50,18 @@ class MySQLExtension extends cli.Extension {
 	    })
 	    .catch((err) => {
                 this.ui.log('MySQL: connection error.', 'yellow');
-                throw new cli.errors.SystemError(err.message);
+                throw new cli.errors.SystemError({
+		    message: err.message,
+		    context: 'Either MySQL is not installed or the entered MySQL host ' + this.databaseConfig.connection.host + ':' + (this.databaseConfig.connection.port || '3306')  + ' is unreachable.',
+		    help: 'Please run `ghost config` and then `ghost setup mysql` again.'
+		});
 	    });
     }
 
     createMySQLUser(ctx) {
         let randomPassword = crypto.randomBytes(10).toString('hex');
         let host = self.databaseConfig.connection.host;
+	let username = 'ghost-';
 
 	return this._query('CREATE USER \'ghost\'@\'' + host + '\' IDENTIFIED BY \'' + randomPassword + '\';')
 	    .then(() => {
@@ -78,7 +83,6 @@ class MySQLExtension extends cli.Extension {
                 this.ui.log('MySQL: unable to create `ghost` user.', 'yellow');
                 throw new cli.errors.SystemError(err.message);
 	    });
-        });
     }
 
     grantPermissions() {
@@ -89,7 +93,7 @@ class MySQLExtension extends cli.Extension {
 	    .then(() => {		
                 this.ui.log('MySQL: successfully granted permissions for `ghost` user.', 'green');
 
-                this._query('FLUSH PRIVILEGES;')
+                return this._query('FLUSH PRIVILEGES;')
 		    .then(() => {
 			this.ui.log('MySQL: flushed privileges', 'green');
 		    })
@@ -97,14 +101,11 @@ class MySQLExtension extends cli.Extension {
 			this.ui.log('MySQL: unable to flush privileges.', 'yellow');
                         throw new cli.errors.SystemError(err.message);
 		    });
-                });
-
 	    })
 	    .catch((err) => {
 		this.ui.log('MySQL: unable to grant permissions for `ghost` user.', 'yellow');
                 throw new cli.errors.SystemError(err.message);
 	    });
-        });
     }
 }
 
