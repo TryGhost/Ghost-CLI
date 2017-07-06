52cdcab7042b995606a3584881647e2c07ff5fdd
Austin Burdine <acburdine@gmail.com>
2017-07-06 00:46:12 -0400

    feat(ssl): refactor ssl setup to use acme.sh rather than greenlock
    no issue
    - greenlock implicitly requires python, acme.sh is much more widely used

--- extensions/nginx/letsencrypt.js
+++ extensions/nginx/letsencrypt.js
@@ -1,20 +1,37 @@
 'use strict';
+const fs = require('fs-extra');
 const url = require('url');
 const path = require('path');
 const execa = require('execa');
+const download = require('download');
 
-const LIVE_URL = 'https://acme-v01.api.letsencrypt.org/directory';
-const STAGING_URL = 'https://acme-staging.api.letsencrypt.org/directory';
+// This is how we will do version control for acme.sh
+const ACME_VERSION = '2.7.2';
 
-module.exports = function letsencrypt(instance, email, staging) {
-    let hostname = url.parse(instance.config.get('url')).hostname;
-    let rootPath = path.resolve(instance.dir, 'system', 'nginx-root');
-    let letsencryptFolder = path.join(instance.dir, 'system', 'letsencrypt');
-    let sslGenArgs = `certonly --agree-tos --email ${email} --webroot --webroot-path ${rootPath}` +
-                    ` --config-dir ${letsencryptFolder} --domains ${hostname} --server ${staging ? STAGING_URL : LIVE_URL}`;
+module.exports = function letsencrypt(instance, email, staging, renew) {
+    let downloadPromise;
+    let acmePath = path.join(__dirname, 'acme.sh');
 
-    return execa('greenlock', sslGenArgs.split(' '), {
-        preferLocal: true,
-        localDir: __dirname
+    if (fs.existsSync(acmePath)) {
+        downloadPromise = Promise.resolve();
+    } else {
+        let acmeUrl = `https://raw.githubusercontent.com/Neilpang/acme.sh/${ACME_VERSION}/acme.sh`;
+        downloadPromise = download(acmeUrl).then(data => fs.writeFile(acmePath, data, {mode: 0o774}));
+    }
+
+    return downloadPromise.then(() => {
+        let hostname = url.parse(instance.config.get('url')).hostname;
+        let rootPath = path.resolve(instance.dir, 'system', 'nginx-root');
+        let letsencryptFolder = path.join(instance.dir, 'system', 'letsencrypt');
+
+        fs.ensureDirSync(letsencryptFolder);
+
+        let fullchain = path.join(letsencryptFolder, 'fullchain.pem');
+        let privkey = path.join(letsencryptFolder, 'privkey.pem');
+
+        let cmd = `${acmePath} --${renew ? 'renew' : 'issue'} --domain ${hostname} --webroot ${rootPath} ` +
+            `--accountemail ${email} --key-file ${privkey} --fullchain-file ${fullchain}${staging ? ' --staging' : ''}`;
+
+        return execa.shell(cmd);
     });
 };
