e4b11d0c771694a8cad6c7e85269d5aed18789d8
Austin Burdine <acburdine@gmail.com>
2017-07-29 22:21:46 -0400

    chore(test): add config command unit tests

---
+++ test/unit/commands/config-spec.js
@@ -0,0 +1,356 @@
+'use strict';
+const expect = require('chai').expect;
+const sinon = require('sinon');
+const proxyquire = require('proxyquire').noCallThru();
+
+const Config = require('../../../lib/utils/config');
+const errors = require('../../../lib/errors');
+
+const modulePath = '../../../lib/commands/config';
+
+describe('Unit: Command > Config', function () {
+    it('constructs instance', function () {
+        let instanceStub = sinon.stub().returns({ instance: true });
+        let ConfigCommand = require(modulePath);
+
+        let config = new ConfigCommand({}, { getInstance: instanceStub });
+
+        expect(instanceStub.calledOnce).to.be.true;
+        expect(config.instance).to.deep.equal({ instance: true });
+    });
+
+    describe('handleAdvancedOptions', function () {
+        it('sets config url to new port if port is different then what url is set to', function () {
+            let ConfigCommand = proxyquire(modulePath, { './advanced': {} });
+            let instanceConfig = new Config('config.json');
+            let saveStub = sinon.stub(instanceConfig, 'save');
+            let getInstanceStub = sinon.stub().returns({ config: instanceConfig });
+            let config = new ConfigCommand({}, { getInstance: getInstanceStub });
+
+            instanceConfig.set('url', 'http://localhost:2368');
+            instanceConfig.set('server.port', 2369);
+
+            return config.handleAdvancedOptions().then(() => {
+                expect(instanceConfig.get('url')).to.equal('http://localhost:2369/');
+                expect(instanceConfig.get('server.port')).to.equal(2369);
+                expect(saveStub.calledOnce).to.be.true;
+            });
+        });
+
+        it('does not change port if port not defined', function () {
+            let ConfigCommand = proxyquire(modulePath, { './advanced': {} });
+            let instanceConfig = new Config('config.json');
+            let saveStub = sinon.stub(instanceConfig, 'save');
+            let getInstanceStub = sinon.stub().returns({ config: instanceConfig });
+            let config = new ConfigCommand({}, { getInstance: getInstanceStub });
+
+            instanceConfig.set('url', 'http://localhost:2368');
+
+            return config.handleAdvancedOptions().then(() => {
+                expect(instanceConfig.get('url')).to.equal('http://localhost:2368');
+                expect(instanceConfig.get('server.port', null)).to.equal(null);
+                expect(saveStub.calledOnce).to.be.true;
+            });
+        });
+
+        it('handles nonexistent values correctly', function () {
+            let ConfigCommand = proxyquire(modulePath, { './advanced': {
+                url: {
+                    defaultValue: 'http://localhost:2368'
+                },
+                port: {
+                    configPath: 'server.port'
+                },
+                log: {
+                    configPath: 'logging.transports',
+                    defaultValue() {
+                        return Promise.resolve(['file', 'stdout']);
+                    }
+                }
+            } });
+            let instanceConfig = new Config('config.json');
+            let saveStub = sinon.stub(instanceConfig, 'save');
+            let getInstanceStub = sinon.stub().returns({ config: instanceConfig });
+            let config = new ConfigCommand({}, { getInstance: getInstanceStub });
+
+            return config.handleAdvancedOptions({log: []}).then(() => {
+                expect(instanceConfig.get('url')).to.equal('http://localhost:2368');
+                expect(instanceConfig.get('server.port')).to.be.undefined;
+                expect(instanceConfig.get('logging.transports')).to.deep.equal(['file', 'stdout']);
+                expect(saveStub.calledOnce).to.be.true;
+            });
+        });
+
+        it('runs the transform function if one is set', function () {
+            let ConfigCommand = proxyquire(modulePath, { './advanced': {
+                url: {
+                    transform: value => value.toLowerCase()
+                }
+            } });
+            let instanceConfig = new Config('config.json');
+            let saveStub = sinon.stub(instanceConfig, 'save');
+            let getInstanceStub = sinon.stub().returns({ config: instanceConfig });
+            let config = new ConfigCommand({}, { getInstance: getInstanceStub });
+
+            return config.handleAdvancedOptions({ url: 'http://MyWebsite.com' }).then(() => {
+                expect(instanceConfig.get('url')).to.equal('http://mywebsite.com');
+                expect(saveStub.calledOnce).to.be.true;
+            });
+        });
+
+        it('throws config error if validate function is defined and doesn\'t return true', function () {
+            let ConfigCommand = proxyquire(modulePath, { './advanced': {
+                url: {
+                    validate: () => 'invalid url'
+                }
+            } });
+            let instanceConfig = new Config('config.json');
+            let getInstanceStub = sinon.stub().returns({ config: instanceConfig });
+            let config = new ConfigCommand({}, { getInstance: getInstanceStub });
+
+            return config.handleAdvancedOptions({ url: 'http://localhost:2368' }).then(() => {
+                expect(false, 'error should have been thrown').to.be.true;
+            }).catch((error) => {
+                expect(error).to.be.an.instanceof(errors.ConfigError);
+                expect(error.options.config).to.deep.equal({url: 'http://localhost:2368'});
+            });
+        });
+    });
+
+    describe('getConfigPrompts', function () {
+        let ConfigCommand = require(modulePath);
+        let getInstanceStub, config, instanceConfig;
+
+        beforeEach(function () {
+            instanceConfig = new Config('config.json');
+            getInstanceStub = sinon.stub().returns({ config: instanceConfig });
+            config = new ConfigCommand({}, { getInstance: getInstanceStub });
+        });
+
+        it('returns no prompts if url is defined and db is sqlite3', function () {
+            let argv = {
+                url: 'someurl.com',
+                db: 'sqlite3'
+            };
+
+            let result = config.getConfigPrompts(argv);
+            expect(result).to.deep.equal([]);
+        });
+
+        it('returns url prompt with validator if url is not provided and db is sqlite3', function () {
+            let argv = { db: 'sqlite3' };
+            let result = config.getConfigPrompts(argv);
+
+            expect(result).to.have.length(1);
+            expect(result[0].name).to.equal('url');
+            expect(result[0].validate('http://localhost:2368')).to.be.true;
+            expect(result[0].validate('notaurl')).to.match(/Invalid URL/);
+        });
+
+        it('returns db prompts if db arg not provided', function () {
+            let argv = { url: 'http://localhost.com' };
+            let result = config.getConfigPrompts(argv);
+
+            expect(result).to.have.length(4);
+            expect(result.find(prompt => prompt.name === 'dbhost')).to.be.ok;
+            expect(result.find(prompt => prompt.name === 'dbuser')).to.be.ok;
+            expect(result.find(prompt => prompt.name === 'dbpass')).to.be.ok;
+            expect(result.find(prompt => prompt.name === 'dbname')).to.be.ok;
+        });
+
+        it('returns db prompts if db arg is not sqlite3', function () {
+            let argv = { url: 'http://localhost.com', db: 'mysql' };
+            instanceConfig.set('database.connection.password', 'password');
+            let result = config.getConfigPrompts(argv);
+
+            expect(result).to.have.length(4);
+            expect(result.find(prompt => prompt.name === 'dbhost')).to.be.ok;
+
+            let userprompt = result.find(prompt => prompt.name === 'dbuser');
+            expect(userprompt).to.be.ok;
+            expect(userprompt.validate('')).to.match(/must supply a MySQL username/);
+            expect(userprompt.validate('root')).to.be.true;
+
+            let passprompt = result.find(prompt => prompt.name === 'dbpass');
+            expect(passprompt).to.be.ok;
+            expect(passprompt.message).to.match(/skip to keep current/);
+
+            expect(result.find(prompt => prompt.name === 'dbname')).to.be.ok;
+        });
+
+        it('doesn\'t return dbhost prompt if dbhost provided', function () {
+            let argv = { url: 'http://localhost.com', db: 'mysql', dbhost: 'localhost' };
+            let result = config.getConfigPrompts(argv);
+
+            expect(result).to.have.length(3);
+            expect(result.find(prompt => prompt.name === 'dbhost')).to.not.be.ok;
+            expect(result.find(prompt => prompt.name === 'dbuser')).to.be.ok;
+            expect(result.find(prompt => prompt.name === 'dbpass')).to.be.ok;
+            expect(result.find(prompt => prompt.name === 'dbname')).to.be.ok;
+        });
+
+        it('doesn\'t return dbuser prompt if dbuser provided', function () {
+            let argv = { url: 'http://localhost.com', db: 'mysql', dbuser: 'root' };
+            let result = config.getConfigPrompts(argv);
+
+            expect(result).to.have.length(3);
+            expect(result.find(prompt => prompt.name === 'dbhost')).to.be.ok;
+            expect(result.find(prompt => prompt.name === 'dbuser')).to.not.be.ok;
+            expect(result.find(prompt => prompt.name === 'dbpass')).to.be.ok;
+            expect(result.find(prompt => prompt.name === 'dbname')).to.be.ok;
+        });
+
+        it('doesn\'t return dbpass prompt if dbpass provided', function () {
+            let argv = { url: 'http://localhost.com', db: 'mysql', dbpass: 'password' };
+            let result = config.getConfigPrompts(argv);
+
+            expect(result).to.have.length(3);
+            expect(result.find(prompt => prompt.name === 'dbhost')).to.be.ok;
+            expect(result.find(prompt => prompt.name === 'dbuser')).to.be.ok;
+            expect(result.find(prompt => prompt.name === 'dbpass')).to.not.be.ok;
+            expect(result.find(prompt => prompt.name === 'dbname')).to.be.ok;
+        });
+
+        it('doesn\'t return dbname prompt if dbname provided', function () {
+            let argv = { url: 'http://localhost.com', db: 'mysql', dbname: 'ghost' };
+            let result = config.getConfigPrompts(argv);
+
+            expect(result).to.have.length(3);
+            expect(result.find(prompt => prompt.name === 'dbhost')).to.be.ok;
+            expect(result.find(prompt => prompt.name === 'dbuser')).to.be.ok;
+            expect(result.find(prompt => prompt.name === 'dbpass')).to.be.ok;
+            expect(result.find(prompt => prompt.name === 'dbname')).to.not.be.ok;
+        });
+    });
+
+    describe('run', function () {
+        let ConfigCommand = require(modulePath);
+
+        it('outputs key if key defined and value is not', function () {
+            let logStub = sinon.stub();
+            let checkEnvironmentStub = sinon.stub();
+            let instanceConfig = new Config('config.json');
+            let getInstanceStub = sinon.stub().returns({checkEnvironment: checkEnvironmentStub, config: instanceConfig});
+            instanceConfig.set('url', 'http://localhost:2368');
+
+            let config = new ConfigCommand({log: logStub}, {getInstance: getInstanceStub});
+
+            return config.run({key: 'url'}).then(() => {
+                expect(checkEnvironmentStub.calledOnce).to.be.true;
+                expect(logStub.calledOnce).to.be.true;
+                expect(logStub.args[0][0]).to.equal('http://localhost:2368');
+
+                return config.run({key: 'nope'});
+            }).then(() => {
+                expect(checkEnvironmentStub.calledTwice).to.be.true;
+                expect(logStub.calledOnce).to.be.true;
+            });
+        });
+
+        it('sets value if both key and value defined', function () {
+            let checkEnvironmentStub = sinon.stub();
+            let instanceConfig = new Config('config.json');
+            let setStub = sinon.stub(instanceConfig, 'set').returns(instanceConfig);
+            let saveStub = sinon.stub(instanceConfig, 'save');
+            let getInstanceStub = sinon.stub().returns({checkEnvironment: checkEnvironmentStub, config: instanceConfig});
+
+            let config = new ConfigCommand({}, {getInstance: getInstanceStub});
+
+            return config.run({key: 'url', value: 'http://localhost:2368'}).then(() => {
+                expect(checkEnvironmentStub.calledOnce).to.be.true;
+                expect(setStub.calledOnce).to.be.true;
+                expect(saveStub.calledOnce).to.be.true;
+                expect(setStub.args[0]).to.deep.equal(['url', 'http://localhost:2368']);
+            });
+        });
+
+        it('calls handleAdvancedOptions without prompts if no-prompt is set', function () {
+            let getInstanceStub = sinon.stub().returns({});
+
+            let config = new ConfigCommand({}, {getInstance: getInstanceStub});
+            let getConfigPromptsStub = sinon.stub(config, 'getConfigPrompts').returns([]);
+            let handleAdvancedOptionsStub = sinon.stub(config, 'handleAdvancedOptions').resolves({ result: true });
+
+            return config.run({ prompt: false }).then((result) => {
+                expect(result).to.deep.equal({ result: true });
+                expect(getConfigPromptsStub.calledOnce).to.be.true;
+                expect(getConfigPromptsStub.args[0][0]).to.deep.equal({ prompt: false });
+                expect(handleAdvancedOptionsStub.calledOnce).to.be.true;
+                expect(handleAdvancedOptionsStub.args[0][0]).to.deep.equal({ prompt: false });
+            });
+        });
+
+        it('calls handleAdvancedOptions without prompts if no prompts are needed', function () {
+            let getInstanceStub = sinon.stub().returns({});
+
+            let config = new ConfigCommand({}, {getInstance: getInstanceStub});
+            let getConfigPromptsStub = sinon.stub(config, 'getConfigPrompts').returns([]);
+            let handleAdvancedOptionsStub = sinon.stub(config, 'handleAdvancedOptions').resolves({ result: true });
+
+            return config.run({ prompt: true }).then((result) => {
+                expect(result).to.deep.equal({ result: true });
+                expect(getConfigPromptsStub.calledOnce).to.be.true;
+                expect(getConfigPromptsStub.args[0][0]).to.deep.equal({ prompt: true });
+                expect(handleAdvancedOptionsStub.calledOnce).to.be.true;
+                expect(handleAdvancedOptionsStub.args[0][0]).to.deep.equal({ prompt: true });
+            });
+        });
+
+        it('prompts with defined prompts, passes result to handleAdvancedOptions', function () {
+            let getInstanceStub = sinon.stub().returns({});
+            let promptStub = sinon.stub().resolves({});
+
+            let config = new ConfigCommand({prompt: promptStub}, {getInstance: getInstanceStub});
+            let getConfigPromptsStub = sinon.stub(config, 'getConfigPrompts').returns([{ name: 'dbhost' }]);
+            let handleAdvancedOptionsStub = sinon.stub(config, 'handleAdvancedOptions').resolves({ result: true });
+
+            return config.run({ prompt: true }).then((result) => {
+                expect(result).to.deep.equal({ result: true });
+                expect(getConfigPromptsStub.calledOnce).to.be.true;
+                expect(getConfigPromptsStub.args[0][0]).to.deep.equal({ prompt: true });
+                expect(promptStub.calledOnce).to.be.true;
+                expect(promptStub.args[0][0]).to.deep.equal([{ name: 'dbhost' }]);
+                expect(handleAdvancedOptionsStub.calledOnce).to.be.true;
+                expect(handleAdvancedOptionsStub.args[0][0]).to.deep.equal({ prompt: true });
+            });
+        });
+
+        it('sets db to mysql if dbhost is provided via prompts', function () {
+            let getInstanceStub = sinon.stub().returns({});
+            let promptStub = sinon.stub().resolves({ dbhost: 'localhost' });
+
+            let config = new ConfigCommand({prompt: promptStub}, {getInstance: getInstanceStub});
+            let getConfigPromptsStub = sinon.stub(config, 'getConfigPrompts').returns([{ name: 'dbhost' }]);
+            let handleAdvancedOptionsStub = sinon.stub(config, 'handleAdvancedOptions').resolves({ result: true });
+
+            return config.run({ prompt: true }).then((result) => {
+                expect(result).to.deep.equal({ result: true });
+                expect(getConfigPromptsStub.calledOnce).to.be.true;
+                expect(getConfigPromptsStub.args[0][0]).to.deep.equal({ prompt: true, db: 'mysql', dbhost: 'localhost' });
+                expect(promptStub.calledOnce).to.be.true;
+                expect(promptStub.args[0][0]).to.deep.equal([{ name: 'dbhost' }]);
+                expect(handleAdvancedOptionsStub.calledOnce).to.be.true;
+                expect(handleAdvancedOptionsStub.args[0][0]).to.deep.equal({ prompt: true, db: 'mysql', dbhost: 'localhost' });
+            });
+        });
+
+        it('doesn\'t add null prompt values to argv object', function () {
+            let getInstanceStub = sinon.stub().returns({});
+            let promptStub = sinon.stub().resolves({ dbhost: 'localhost', dbpass: '' });
+
+            let config = new ConfigCommand({prompt: promptStub}, {getInstance: getInstanceStub});
+            let getConfigPromptsStub = sinon.stub(config, 'getConfigPrompts').returns([{ name: 'dbhost' }]);
+            let handleAdvancedOptionsStub = sinon.stub(config, 'handleAdvancedOptions').resolves({ result: true });
+
+            return config.run({ prompt: true }).then((result) => {
+                expect(result).to.deep.equal({ result: true });
+                expect(getConfigPromptsStub.calledOnce).to.be.true;
+                expect(getConfigPromptsStub.args[0][0]).to.deep.equal({ prompt: true, db: 'mysql', dbhost: 'localhost' });
+                expect(promptStub.calledOnce).to.be.true;
+                expect(promptStub.args[0][0]).to.deep.equal([{ name: 'dbhost' }]);
+                expect(handleAdvancedOptionsStub.calledOnce).to.be.true;
+                expect(handleAdvancedOptionsStub.args[0][0]).to.deep.equal({ prompt: true, db: 'mysql', dbhost: 'localhost' });
+            });
+        });
+    });
+});
