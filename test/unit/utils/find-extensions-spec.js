47db08e84415922e321cf7c38e8d61fc0950bd85
Vikas Potluri <potlurivikas@gmail.com>
2018-02-05 01:42:41 -0600

    chore(tests): finish find extensions util

--- test/unit/utils/find-extensions-spec.js
+++ test/unit/utils/find-extensions-spec.js
@@ -1,8 +1,62 @@
 'use strict';
 
+const expect = require('chai').expect;
+const sinon = require('sinon');
+const proxyquire = require('proxyquire').noCallThru();
+
 const modulePath = '../../../lib/utils/find-extensions';
 
-// TODO: remove line once tests are implemented
-require(modulePath);
+const localExtensions = [
+    'linux',
+    'nginx',
+    'mysql',
+    'systemd'
+];
 
-describe.skip('Unit: Utils > find-extensions', function () {});
+describe('Unit: Utils > find-extensions', function () {
+    let findExtensions, findStub;
+
+    beforeEach(() => {
+        findStub = sinon.stub().returns([
+            {
+                pkg: {name: 'test'}
+            }, {
+                pkg: {
+                    'ghost-cli': {name: 'rest'}
+                }
+            }, {
+                pkg: {}
+            }
+        ]);
+
+        findExtensions = proxyquire(modulePath, {
+            'find-plugins': findStub,
+            execa: {shellSync: () => ({stdout: '.'})}
+        });
+    });
+
+    it('calls find-plugins with proper args', function () {
+        findExtensions();
+        expect(findStub.calledOnce).to.be.true;
+        const args = findStub.getCall(0).args[0];
+
+        const expected = {
+            keyword: 'ghost-cli-extension',
+            configName: 'ghost-cli',
+            scanAllDirs: true,
+            dir: '.',
+            sort: true
+        };
+
+        const extensions = args.include.map((ext) => ext.split('extensions/')[1]);
+        delete args.include;
+        expect(extensions).to.deep.equal(localExtensions);
+        expect(args).to.deep.equal(expected);
+    });
+
+    it('generates proper extension names', function () {
+        const names = findExtensions().map((ext) => ext.name);
+        const expectedNames = ['test', 'rest', undefined];
+        expect(names).to.deep.equal(expectedNames);
+    });
+});
