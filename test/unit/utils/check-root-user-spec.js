8fc693a777492804e2cee7959df45daa0da6b96a
Austin Burdine <acburdine@gmail.com>
2018-02-07 11:49:08 +0100

    fix(root-user): only skip exiting inside root install
    no issue
    - if outside of a root install (or a root install has been fixed),
    commands will still fail as root

--- test/unit/utils/check-root-user-spec.js
+++ test/unit/utils/check-root-user-spec.js
@@ -167,28 +167,6 @@ describe('Unit: Utils > checkRootUser', function () {
         }
     });
 
-    it('throws error command run with root for non-root installs, but doesn\'t exit on `restart`', function () {
-        const osStub = sandbox.stub(os, 'platform').returns('linux');
-        const cwdStub = sandbox.stub(process, 'cwd').returns('/var/www/ghost');
-        const fsStub = sandbox.stub(fs, 'existsSync');
-        const fsStatStub = sandbox.stub(fs, 'statSync').returns({uid: 501});
-        const processStub = sandbox.stub(process, 'getuid').returns(0);
-        const exitStub = sandbox.stub(process, 'exit').throws();
-        const errorStub = sandbox.stub(console, 'error');
-
-        fsStub.withArgs('/root/.digitalocean_password').returns(false);
-        fsStub.withArgs('/var/www/ghost/.ghost-cli').returns(true);
-
-        checkRootUser('restart');
-        expect(cwdStub.calledOnce).to.be.true;
-        expect(fsStatStub.calledWithExactly('/var/www/ghost/.ghost-cli')).to.be.true;
-        expect(osStub.calledOnce).to.be.true;
-        expect(processStub.calledOnce).to.be.true;
-        expect(errorStub.calledOnce).to.be.true;
-        expect(exitStub.calledOnce).to.be.false;
-        expect(errorStub.args[0][0]).to.match(/Can't run command as 'root' user/);
-    });
-
     it('throws error command run with root outside of valid ghost installation', function () {
         const osStub = sandbox.stub(os, 'platform').returns('linux');
         const cwdStub = sandbox.stub(process, 'cwd').returns('/var/www/');
@@ -215,28 +193,5 @@ describe('Unit: Utils > checkRootUser', function () {
             expect(exitStub.calledOnce).to.be.true;
             expect(errorStub.args[0][0]).to.match(/Can't run command as 'root' user/);
         }
-    });
-
-    it('throws error command run with root outside of valid ghost installation, but doesn\'t exit on `restart`', function () {
-        const osStub = sandbox.stub(os, 'platform').returns('linux');
-        const cwdStub = sandbox.stub(process, 'cwd').returns('/var/www/');
-        const fsStub = sandbox.stub(fs, 'existsSync');
-        const fsStatStub = sandbox.stub(fs, 'statSync').returns({uid: 501});
-        const processStub = sandbox.stub(process, 'getuid').returns(0);
-        const exitStub = sandbox.stub(process, 'exit').throws();
-        const errorStub = sandbox.stub(console, 'error');
-
-        fsStub.withArgs('/root/.digitalocean_password').returns(false);
-        fsStub.withArgs('/var/www/.ghost-cli').returns(false);
-
-        checkRootUser('restart');
-        expect(cwdStub.calledOnce).to.be.true;
-        expect(fsStub.calledWithExactly('/var/www/.ghost-cli')).to.be.true;
-        expect(fsStatStub.calledOnce).to.be.false;
-        expect(osStub.calledOnce).to.be.true;
-        expect(processStub.calledOnce).to.be.true;
-        expect(errorStub.calledOnce).to.be.true;
-        expect(exitStub.calledOnce).to.be.false;
-        expect(errorStub.args[0][0]).to.match(/Can't run command as 'root' user/);
     });
 });
