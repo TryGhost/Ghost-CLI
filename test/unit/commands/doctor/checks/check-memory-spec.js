be90bd2047a90c480574289bacb3b14c120bdbd1
Vikas Potluri <potlurivikas@gmail.com>
2018-03-27 11:26:40 -0500

    fix(doctor): Use systeminformation for memory availability
    os.freemem() doesn't account for memory used in buffers / caches in
    linux, so it doesn't give an accurate reading of the available memory of
    a system

--- test/unit/commands/doctor/checks/check-memory-spec.js
+++ test/unit/commands/doctor/checks/check-memory-spec.js
@@ -2,7 +2,7 @@
 const expect = require('chai').expect;
 const sinon = require('sinon');
 
-const os = require('os');
+const sysinfo = require('systeminformation');
 const errors = require('../../../../../lib/errors');
 
 const modulePath = '../../../../../lib/commands/doctor/checks/check-memory';
@@ -34,29 +34,34 @@ describe('Unit: Doctor Checks > Memory', function () {
         expect(memCheck.enabled(ctx)).to.be.true;
     });
 
-    it('fails if not enough memory is available', function () {
-        const osStub = sandbox.stub(os, 'freemem').returns(10);
+    it('uses systeminformation to determine memory availability', function () {
+        const memStub = sandbox.stub(sysinfo, 'mem').rejects(new Error('systeminformation'));
         const memCheck = require(modulePath);
-        const ctx = {
-            argv: {'check-mem': true}
-        };
 
-        return memCheck.task(ctx).catch((error) => {
+        return memCheck.task().catch(error => {
+            expect(error).to.be.an('error');
+            expect(error.message).to.equal('systeminformation');
+            expect(memStub.calledOnce).to.be.true;
+        });
+    });
+
+    it('fails if not enough memory is available', function () {
+        const memStub = sandbox.stub(sysinfo, 'mem').resolves({available: 10});
+        const memCheck = require(modulePath);
+
+        return memCheck.task().catch((error) => {
             expect(error).to.be.an.instanceof(errors.SystemError);
             expect(error.message).to.match(/MB of memory available for smooth operation/);
-            expect(osStub.calledOnce).to.be.true;
+            expect(memStub.calledOnce).to.be.true;
         });
     });
 
     it('passes if there is enough memory', function () {
-        const osStub = sandbox.stub(os, 'freemem').returns(157286400);
+        const memStub = sandbox.stub(sysinfo, 'mem').resolves({available: 157286400});
         const memCheck = require(modulePath);
-        const ctx = {
-            argv: {'check-mem': true}
-        };
 
-        return memCheck.task(ctx).then(() => {
-            expect(osStub.calledOnce).to.be.true;
+        return memCheck.task().then(() => {
+            expect(memStub.calledOnce).to.be.true;
         });
     });
 });
