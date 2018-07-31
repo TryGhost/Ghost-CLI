ff61141b823b2018617c3163bb713102c798c310
Katharina Irrgang <katharina.irrgang@googlemail.com>
2018-07-31 11:03:01 +0200

    feat(v2): update knex-migrator execution for Ghost 2.0 (#765)
    refs #759
    
    - We have moved the execution of knex-migrator into Ghost 2.0.0
    - This commit will ensure we skip the db migration when you:
      - migrate from ^1 to ^2
      - you update from ^2 to ^2
      - when you install ^2
    - Added net socket server for Ghost 2.0 (alternative to simple port polling)
    - way better error handling between Ghost and the CLI
    - Ghost 2.0 executes knex-migrator
      - it will turn maintenance on if migrations need to be executed
      - the handling of receiving success or failure state requires a better communication between the CLI and Ghost, because the blog stays in maintenance mode and runs the migrations in background
    - Ghost will tell the CLI when it's ready by using an extension: write a socket url into the config and send the success/failure state
    - this is much better than using the http socket to communicate, because
      - A) port polling connects to the http port, it's impossible to send simple messages over this transport layer
      - B) the code is much simpler, CLI opens a socket port and Ghost pushes a notification if the notification is available
      - C) we receive any error from Ghost - even if the http server wasn't started yet
    - we don't communicate with Ghost, Ghost communicates with the CLI
    - port polling for v1 blogs is untouched, still works as expected
    - coverage has decreased a very little 0,2% - will try to add more tests when we merge the 1.9 branch into master

--- test/unit/utils/port-polling-spec.js
+++ test/unit/utils/port-polling-spec.js
@@ -20,178 +20,338 @@ describe('Unit: Utils > portPolling', function () {
             });
     });
 
-    it('Ghost does never start', function () {
-        const netStub = sinon.stub();
+    describe('useNetServer is enabled', function () {
+        it('Ghost does start', function () {
+            const netStub = sinon.stub();
+            const socketStub = sinon.stub();
 
-        netStub.setTimeout = sinon.stub();
-        netStub.destroy = sinon.stub();
-        netStub.on = function (event, cb) {
-            if (event === 'error') {
-                cb(new Error('whoops'));
-            }
-        };
-
-        const connectStub = sinon.stub(net, 'connect').returns(netStub);
-
-        return portPolling({port: 1111, maxTries: 3, timeoutInMS: 100})
-            .then(() => {
-                throw new Error('Expected error');
-            })
-            .catch((err) => {
-                expect(err.options.suggestion).to.exist;
-                expect(err.message).to.eql('Ghost did not start.');
-                expect(err.err.message).to.eql('whoops');
-                expect(connectStub.callCount).to.equal(4);
-                expect(connectStub.calledWithExactly(1111, 'localhost'), 'uses localhost by default').to.be.true;
-                expect(netStub.destroy.callCount).to.eql(4);
+            socketStub.on = sinon.stub().callsFake((event, cb) => {
+                if (event === 'data') {
+                    cb(JSON.stringify({started: true}));
+                }
             });
-    });
 
-    it('Ghost does start, but falls over', function () {
-        const netStub = sinon.stub();
+            socketStub.destroy = sinon.stub();
 
-        netStub.setTimeout = sinon.stub();
-        netStub.destroy = sinon.stub();
-
-        let i = 0;
-        netStub.on = function (event, cb) {
-            i = i + 1;
-
-            if (event === 'close') {
+            netStub.listen = sinon.stub();
+            netStub.close = sinon.stub().callsFake((cb)=> {
                 cb();
-            } else if (event === 'error' && i === 3) {
-                cb(new Error());
-            } else if (event === 'connect' && i === 5) {
-                cb();
-            }
-        };
-
-        const connectStub = sinon.stub(net, 'connect').returns(netStub);
-
-        return portPolling({port: 1111, maxTries: 3, timeoutInMS: 100, delayOnConnectInMS: 150, host: '0.0.0.0'})
-            .then(() => {
-                throw new Error('Expected error');
-            })
-            .catch((err) => {
-                expect(err.options.suggestion).to.exist;
-                expect(err.message).to.eql('Ghost did not start.');
-                expect(connectStub.calledTwice).to.be.true;
-                expect(connectStub.calledWithExactly(1111, 'localhost'), 'uses localhost if host is 0.0.0.0').to.be.true;
-                expect(netStub.destroy.callCount).to.eql(2);
             });
-    });
 
-    it('Ghost does start', function () {
-        const netStub = sinon.stub();
-
-        netStub.setTimeout = sinon.stub();
-        netStub.destroy = sinon.stub();
-
-        let i = 0;
-        netStub.on = function (event, cb) {
-            i = i + 1;
-
-            if (i === 6) {
-                expect(event).to.eql('close');
-            } else if (i === 5 && event === 'connect') {
-                cb();
-            } else if (i === 3 && event === 'error') {
-                cb(new Error());
-            }
-        };
-
-        const connectStub = sinon.stub(net, 'connect').returns(netStub);
-
-        return portPolling({port: 1111, maxTries: 3, timeoutInMS: 100, delayOnConnectInMS: 150, host: '10.0.1.0'})
-            .then(() => {
-                expect(connectStub.calledTwice).to.be.true;
-                expect(connectStub.calledWithExactly(1111, '10.0.1.0'), 'uses custom host').to.be.true;
-                expect(netStub.destroy.callCount).to.eql(2);
-            })
-            .catch((err) => {
-                throw err;
-            });
-    });
-
-    it('Ghost does start, skip delay on connect', function () {
-        const netStub = sinon.stub();
-
-        netStub.setTimeout = sinon.stub();
-        netStub.destroy = sinon.stub();
-
-        netStub.on = function (event, cb) {
-            expect(event).to.not.eql('close');
-
-            if (event === 'connect') {
-                cb();
-            }
-        };
-
-        sinon.stub(net, 'connect').returns(netStub);
-
-        return portPolling({port: 1111, maxTries: 3, timeoutInMS: 100, delayOnConnectInMS: false})
-            .then(() => {
-                expect(netStub.destroy.callCount).to.eql(1);
-            })
-            .catch((err) => {
-                throw err;
-            });
-    });
-
-    it('socket times out', function () {
-        const netStub = sinon.stub();
-
-        netStub.setTimeout = sinon.stub();
-        netStub.destroy = sinon.stub();
-
-        netStub.on = function (event, cb) {
-            if (event === 'timeout') {
-                cb();
-            }
-        };
-
-        sinon.stub(net, 'connect').returns(netStub);
-
-        return portPolling({port: 1111, maxTries: 3, timeoutInMS: 100, socketTimeoutInMS: 300})
-            .then(() => {
-                throw new Error('Expected error');
-            })
-            .catch((err) => {
-                expect(err.options.suggestion).to.exist;
-                expect(err.message).to.eql('Ghost did not start.');
-                expect(netStub.destroy.callCount).to.eql(4);
-            });
-    });
-
-    it('Ghost connects, but socket times out kicks in', function () {
-        const netStub = sinon.stub();
-
-        netStub.setTimeout = sinon.stub();
-        netStub.destroy = sinon.stub();
-
-        const events = {};
-        netStub.on = function (event, cb) {
-            if (event === 'connect') {
-                cb();
-
+            sinon.stub(net, 'createServer').callsFake((fn) => {
                 setTimeout(() => {
-                    events.timeout();
+                    fn(socketStub);
                 }, 100);
-            }
 
-            events[event] = cb;
-        };
-
-        sinon.stub(net, 'connect').returns(netStub);
-
-        return portPolling({port: 1111, maxTries: 2, timeoutInMS: 100, socketTimeoutInMS: 300})
-            .then(() => {
-                throw new Error('Expected error');
-            })
-            .catch((err) => {
-                expect(err.options.suggestion).to.exist;
-                expect(err.message).to.eql('Ghost did not start.');
-                expect(netStub.destroy.callCount).to.eql(3);
+                return netStub;
             });
+
+            return portPolling({
+                netServerTimeoutInMS: 1000,
+                useNetServer: true
+            }).then(() => {
+                expect(net.createServer.calledOnce).to.be.true;
+                expect(netStub.listen.callCount).to.eql(1);
+                expect(netStub.listen.calledWithExactly({host: 'localhost', port: 1212})).to.be.true;
+                expect(netStub.close.callCount).to.eql(1);
+
+                expect(socketStub.destroy.callCount).to.eql(1);
+                expect(socketStub.on.callCount).to.eql(1);
+            }).catch((err) => {
+                throw err;
+            });
+        });
+
+        it('Ghost didn\'t start', function () {
+            const netStub = sinon.stub();
+            const socketStub = sinon.stub();
+
+            socketStub.on = sinon.stub().callsFake((event, cb) => {
+                if (event === 'data') {
+                    cb(JSON.stringify({false: true, error: {message: 'Syntax Error'}}));
+                }
+            });
+
+            socketStub.destroy = sinon.stub();
+
+            netStub.listen = sinon.stub();
+            netStub.close = sinon.stub().callsFake((cb)=> {
+                cb();
+            });
+
+            sinon.stub(net, 'createServer').callsFake((fn) => {
+                setTimeout(() => {
+                    fn(socketStub);
+                }, 100);
+
+                return netStub;
+            });
+
+            return portPolling({
+                netServerTimeoutInMS: 1000,
+                useNetServer: true
+            }).then(() => {
+                expect('1').to.equal(1, 'Ghost should not start.');
+            }).catch((err) => {
+                expect(err.message).to.eql('Syntax Error');
+                expect(net.createServer.calledOnce).to.be.true;
+                expect(netStub.listen.calledOnce).to.be.true;
+                expect(netStub.listen.calledWithExactly({host: 'localhost', port: 1212})).to.be.true;
+                expect(netStub.close.callCount).to.eql(1);
+
+                expect(socketStub.destroy.callCount).to.eql(1);
+                expect(socketStub.on.callCount).to.eql(1);
+            });
+        });
+
+        it('Ghost does not communicate, expect timeout', function () {
+            const netStub = sinon.stub();
+            const socketStub = sinon.stub();
+
+            socketStub.on = sinon.stub();
+            socketStub.destroy = sinon.stub();
+
+            netStub.listen = sinon.stub();
+            netStub.close = sinon.stub().callsFake((cb)=> {
+                cb();
+            });
+
+            sinon.stub(net, 'createServer').callsFake(() => {
+                return netStub;
+            });
+
+            return portPolling({
+                netServerTimeoutInMS: 500,
+                useNetServer: true
+            }).then(() => {
+                expect('1').to.equal(1, 'Ghost should not start.');
+            }).catch((err) => {
+                expect(err.message).to.eql('Could not communicate with Ghost');
+                expect(net.createServer.calledOnce).to.be.true;
+                expect(netStub.listen.calledOnce).to.be.true;
+                expect(netStub.listen.calledWithExactly({host: 'localhost', port: 1212})).to.be.true;
+                expect(netStub.close.callCount).to.eql(1);
+
+                expect(socketStub.destroy.callCount).to.eql(0);
+                expect(socketStub.on.callCount).to.eql(0);
+            });
+        });
+
+        it('Ghost does not answer, expect timeout', function () {
+            const netStub = sinon.stub();
+            const socketStub = sinon.stub();
+
+            socketStub.on = sinon.stub();
+            socketStub.destroy = sinon.stub();
+
+            netStub.listen = sinon.stub();
+            netStub.close = sinon.stub().callsFake((cb)=> {
+                cb();
+            });
+
+            sinon.stub(net, 'createServer').callsFake((fn) => {
+                setTimeout(() => {
+                    fn(socketStub);
+                }, 100);
+
+                return netStub;
+            });
+
+            return portPolling({
+                netServerTimeoutInMS: 500,
+                useNetServer: true
+            }).then(() => {
+                expect('1').to.equal(1, 'Ghost should not start.');
+            }).catch((err) => {
+                expect(err.message).to.eql('Could not communicate with Ghost');
+                expect(net.createServer.calledOnce).to.be.true;
+                expect(netStub.listen.calledOnce).to.be.true;
+                expect(netStub.listen.calledWithExactly({host: 'localhost', port: 1212})).to.be.true;
+                expect(netStub.close.callCount).to.eql(1);
+
+                expect(socketStub.destroy.callCount).to.eql(1);
+                expect(socketStub.on.callCount).to.eql(1);
+            });
+        });
+    });
+
+    describe('useNetServer is disabled', function () {
+        it('Ghost does never start', function () {
+            const netStub = sinon.stub();
+
+            netStub.setTimeout = sinon.stub();
+            netStub.destroy = sinon.stub();
+            netStub.on = function (event, cb) {
+                if (event === 'error') {
+                    cb(new Error('whoops'));
+                }
+            };
+
+            const connectStub = sinon.stub(net, 'connect').returns(netStub);
+
+            return portPolling({port: 1111, maxTries: 3, retryTimeoutInMS: 100})
+                .then(() => {
+                    throw new Error('Expected error');
+                })
+                .catch((err) => {
+                    expect(err.options.suggestion).to.exist;
+                    expect(err.message).to.eql('Ghost did not start.');
+                    expect(err.err.message).to.eql('whoops');
+                    expect(connectStub.callCount).to.equal(4);
+                    expect(connectStub.calledWithExactly(1111, 'localhost'), 'uses localhost by default').to.be.true;
+                    expect(netStub.destroy.callCount).to.eql(4);
+                });
+        });
+
+        it('Ghost does start, but falls over', function () {
+            const netStub = sinon.stub();
+
+            netStub.setTimeout = sinon.stub();
+            netStub.destroy = sinon.stub();
+
+            let i = 0;
+            netStub.on = function (event, cb) {
+                i = i + 1;
+
+                if (event === 'close') {
+                    cb();
+                } else if (event === 'error' && i === 3) {
+                    cb(new Error());
+                } else if (event === 'connect' && i === 5) {
+                    cb();
+                }
+            };
+
+            const connectStub = sinon.stub(net, 'connect').returns(netStub);
+
+            return portPolling({port: 1111, maxTries: 3, retryTimeoutInMS: 100, delayOnConnectInMS: 150, host: '0.0.0.0'})
+                .then(() => {
+                    throw new Error('Expected error');
+                })
+                .catch((err) => {
+                    expect(err.options.suggestion).to.exist;
+                    expect(err.message).to.eql('Ghost did not start.');
+                    expect(err.err.message).to.eql('Ghost died.');
+                    expect(connectStub.calledTwice).to.be.true;
+                    expect(connectStub.calledWithExactly(1111, 'localhost'), 'uses localhost if host is 0.0.0.0').to.be.true;
+                    expect(netStub.destroy.callCount).to.eql(2);
+                });
+        });
+
+        it('Ghost does start', function () {
+            const netStub = sinon.stub();
+
+            netStub.setTimeout = sinon.stub();
+            netStub.destroy = sinon.stub();
+
+            let i = 0;
+            netStub.on = function (event, cb) {
+                i = i + 1;
+
+                if (i === 6) {
+                    expect(event).to.eql('close');
+                } else if (i === 5 && event === 'connect') {
+                    cb();
+                } else if (i === 3 && event === 'error') {
+                    cb(new Error());
+                }
+            };
+
+            const connectStub = sinon.stub(net, 'connect').returns(netStub);
+
+            return portPolling({port: 1111, maxTries: 3, retryTimeoutInMS: 100, delayOnConnectInMS: 150, host: '10.0.1.0'})
+                .then(() => {
+                    expect(connectStub.calledTwice).to.be.true;
+                    expect(connectStub.calledWithExactly(1111, '10.0.1.0'), 'uses custom host').to.be.true;
+                    expect(netStub.destroy.callCount).to.eql(2);
+                })
+                .catch((err) => {
+                    throw err;
+                });
+        });
+
+        it('Ghost does start, skip delay on connect', function () {
+            const netStub = sinon.stub();
+
+            netStub.setTimeout = sinon.stub();
+            netStub.destroy = sinon.stub();
+
+            netStub.on = function (event, cb) {
+                expect(event).to.not.eql('close');
+
+                if (event === 'connect') {
+                    cb();
+                }
+            };
+
+            sinon.stub(net, 'connect').returns(netStub);
+
+            return portPolling({port: 1111, maxTries: 3, retryTimeoutInMS: 100, delayOnConnectInMS: false})
+                .then(() => {
+                    expect(netStub.destroy.callCount).to.eql(1);
+                })
+                .catch((err) => {
+                    throw err;
+                });
+        });
+
+        it('socket times out', function () {
+            const netStub = sinon.stub();
+
+            netStub.setTimeout = sinon.stub();
+            netStub.destroy = sinon.stub();
+
+            netStub.on = function (event, cb) {
+                if (event === 'timeout') {
+                    cb();
+                }
+            };
+
+            sinon.stub(net, 'connect').returns(netStub);
+
+            return portPolling({port: 1111, maxTries: 3, retryTimeoutInMS: 100, socketTimeoutInMS: 300})
+                .then(() => {
+                    throw new Error('Expected error');
+                })
+                .catch((err) => {
+                    expect(err.options.suggestion).to.exist;
+                    expect(err.message).to.eql('Ghost did not start.');
+                    expect(err.err.message).to.eql('Socket timed out.');
+                    expect(netStub.destroy.callCount).to.eql(4);
+                });
+        });
+
+        it('Ghost connects, but socket times out kicks in', function () {
+            const netStub = sinon.stub();
+
+            netStub.setTimeout = sinon.stub();
+            netStub.destroy = sinon.stub();
+
+            const events = {};
+            netStub.on = function (event, cb) {
+                if (event === 'connect') {
+                    cb();
+
+                    setTimeout(() => {
+                        events.timeout();
+                    }, 100);
+                }
+
+                events[event] = cb;
+            };
+
+            sinon.stub(net, 'connect').returns(netStub);
+
+            return portPolling({port: 1111, maxTries: 2, retryTimeoutInMS: 100, socketTimeoutInMS: 300})
+                .then(() => {
+                    throw new Error('Expected error');
+                })
+                .catch((err) => {
+                    expect(err.options.suggestion).to.exist;
+                    expect(err.message).to.eql('Ghost did not start.');
+                    expect(err.err.message).to.eql('Socket timed out.');
+                    expect(netStub.destroy.callCount).to.eql(3);
+                });
+        });
     });
 });
