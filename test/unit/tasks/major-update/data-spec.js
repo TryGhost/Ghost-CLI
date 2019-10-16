62f9374543a3a672da96ae3c55be7f731608365a
Austin Burdine <acburdine@gmail.com>
2019-10-16 07:16:08 -0400

    refactor(major-update): update major-update to use async/await

--- test/unit/tasks/major-update/data-spec.js
+++ test/unit/tasks/major-update/data-spec.js
@@ -2,7 +2,7 @@
 const expect = require('chai').expect;
 const sinon = require('sinon');
 const proxyquire = require('proxyquire');
-const errors = require('../../../../lib/errors');
+const {CliError} = require('../../../../lib/errors');
 
 describe('Unit: Tasks > Major Update > Data', function () {
     let knexMock, gscanMock, data, connection;
@@ -10,9 +10,7 @@ describe('Unit: Tasks > Major Update > Data', function () {
     beforeEach(function () {
         connection = sinon.stub();
         connection.raw = sinon.stub();
-        connection.destroy = sinon.stub().callsFake((cb) => {
-            cb();
-        });
+        connection.destroy = sinon.stub().resolves();
 
         knexMock = sinon.stub().returns(connection);
 
@@ -29,87 +27,97 @@ describe('Unit: Tasks > Major Update > Data', function () {
         });
     });
 
-    it('requires `options.dir`', function () {
-        return data().catch((err) => {
-            expect(err).to.be.an.instanceOf(errors.CliError);
-        });
+    it('requires `options.dir`', async function () {
+        try {
+            await data({});
+        } catch (err) {
+            expect(err).to.be.an.instanceOf(CliError);
+            return;
+        }
+
+        expect.fail('should have thrown an error');
     });
 
-    it('requires `options.database`', function () {
-        return data({dir: 'dir'}).catch((err) => {
-            expect(err).to.be.an.instanceOf(errors.CliError);
-        });
+    it('requires `options.database`', async function () {
+        try {
+            await data({dir: 'dir'});
+        } catch (err) {
+            expect(err).to.be.an.instanceOf(CliError);
+            return;
+        }
+
+        expect.fail('should have thrown an error');
     });
 
-    it('sqlite3: success', function () {
-        connection.raw.withArgs('SELECT * FROM settings WHERE `key`="active_theme";').resolves([{value: 'casper'}]);
-        connection.raw.withArgs('SELECT uuid FROM posts WHERE slug="v2-demo-post";').resolves([{uuid: 'uuid'}]);
+    it('sqlite3: success', async function () {
+        connection.raw.withArgs('SELECT value FROM settings WHERE `key` = "active_theme";').resolves([{value: 'casper'}]);
+        connection.raw.withArgs('SELECT uuid FROM posts WHERE `slug` = "v2-demo-post";').resolves([{uuid: 'uuid'}]);
 
         gscanMock.check.resolves({unformatted: true});
         gscanMock.format.returns({formatted: true});
 
-        return data({
+        const response = await data({
             database: {
                 client: 'sqlite3'
             },
             dir: '/var/www/ghost/',
-            version: 'versions/2.0.0/'
-        }).then((response) => {
-            expect(response.gscanReport.formatted).to.be.true;
-            expect(response.demoPost.uuid).to.eql('uuid');
-
-            expect(connection.destroy.calledOnce).to.be.true;
-            expect(connection.raw.calledTwice).to.be.true;
-
-            expect(gscanMock.check.calledOnce).to.be.true;
-            expect(gscanMock.format.calledOnce).to.be.true;
-
-            expect(knexMock.calledOnce).to.be.true;
+            versionFolder: 'versions/2.0.0/'
         });
+
+        expect(response.gscanReport.formatted).to.be.true;
+        expect(response.demoPost.uuid).to.eql('uuid');
+
+        expect(connection.destroy.calledOnce).to.be.true;
+        expect(connection.raw.calledTwice).to.be.true;
+
+        expect(gscanMock.check.calledOnce).to.be.true;
+        expect(gscanMock.format.calledOnce).to.be.true;
+
+        expect(knexMock.calledOnce).to.be.true;
     });
 
-    it('mysql: success', function () {
-        connection.raw.withArgs('SELECT * FROM settings WHERE `key`="active_theme";').resolves([[{value: 'not-casper'}]]);
-        connection.raw.withArgs('SELECT uuid FROM posts WHERE slug="v2-demo-post";').resolves([[{uuid: 'uuid'}]]);
+    it('mysql: success', async function () {
+        connection.raw.withArgs('SELECT value FROM settings WHERE `key` = "active_theme";').resolves([[{value: 'not-casper'}]]);
+        connection.raw.withArgs('SELECT uuid FROM posts WHERE `slug` = "v2-demo-post";').resolves([[{uuid: 'uuid'}]]);
 
         gscanMock.check.resolves({unformatted: true});
         gscanMock.format.returns({formatted: true});
 
-        return data({
+        const response = await data({
             database: {
                 client: 'mysql'
             },
             dir: '/var/www/ghost/',
-            version: 'versions/2.0.0/'
-        }).then((response) => {
-            expect(response.gscanReport.formatted).to.be.true;
-            expect(response.demoPost.uuid).to.eql('uuid');
-
-            expect(connection.destroy.calledOnce).to.be.true;
-            expect(connection.raw.calledTwice).to.be.true;
-
-            expect(gscanMock.check.calledOnce).to.be.true;
-            expect(gscanMock.format.calledOnce).to.be.true;
-
-            expect(knexMock.calledOnce).to.be.true;
+            versionFolder: 'versions/2.0.0/'
         });
+
+        expect(response.gscanReport.formatted).to.be.true;
+        expect(response.demoPost.uuid).to.eql('uuid');
+
+        expect(connection.destroy.calledOnce).to.be.true;
+        expect(connection.raw.calledTwice).to.be.true;
+
+        expect(gscanMock.check.calledOnce).to.be.true;
+        expect(gscanMock.format.calledOnce).to.be.true;
+
+        expect(knexMock.calledOnce).to.be.true;
     });
 
-    it('fails', function () {
-        connection.raw.withArgs('SELECT * FROM settings WHERE `key`="active_theme";').resolves([[{value: 'casper'}]]);
-        connection.raw.withArgs('SELECT uuid FROM posts WHERE slug="v2-demo-post";').resolves([[{uuid: 'uuid'}]]);
+    it('fails', async function () {
+        connection.raw.withArgs('SELECT value FROM settings WHERE `key` = "active_theme";').resolves([[{value: 'casper'}]]);
+        connection.raw.withArgs('SELECT uuid FROM posts WHERE `slug` = "v2-demo-post";').resolves([[{uuid: 'uuid'}]]);
 
         gscanMock.check.rejects(new Error('oops'));
 
-        return data({
-            database: {
-                client: 'mysql'
-            },
-            dir: '/var/www/ghost/',
-            version: 'versions/2.0.0/'
-        }).then(() => {
-            expect('1').to.eql(1, 'Expected error');
-        }).catch((err) => {
+        try {
+            await data({
+                database: {
+                    client: 'mysql'
+                },
+                dir: '/var/www/ghost/',
+                versionFolder: 'versions/2.0.0/'
+            });
+        } catch (err) {
             expect(err.message).to.eql('oops');
             expect(connection.destroy.calledOnce).to.be.true;
             expect(connection.raw.calledOnce).to.be.true;
@@ -118,6 +126,9 @@ describe('Unit: Tasks > Major Update > Data', function () {
             expect(gscanMock.format.called).to.be.false;
 
             expect(knexMock.calledOnce).to.be.true;
-        });
+            return;
+        }
+
+        expect.fail('should have thrown an error');
     });
 });
