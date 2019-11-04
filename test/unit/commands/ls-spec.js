a3b0b54ac246e3680a33b4dd53b38149aae421af
Austin Burdine <acburdine@gmail.com>
2019-11-04 15:15:55 +0700

    cleanup: refactor more commands to use async/await

--- test/unit/commands/ls-spec.js
+++ test/unit/commands/ls-spec.js
@@ -6,7 +6,7 @@ const stripAnsi = require('strip-ansi');
 const LsCommand = require('../../../lib/commands/ls');
 
 describe('Unit: Commands > ls', function () {
-    it('outputs the correct data for instances that are running and not running', function () {
+    it('outputs the correct data for instances that are running and not running', async function () {
         const summaryStub = sinon.stub();
         summaryStub.onFirstCall().resolves({
             name: 'testa',
@@ -43,39 +43,37 @@ describe('Unit: Commands > ls', function () {
 
         const instance = new LsCommand({table: tableStub}, {getAllInstances: getAllInstancesStub});
 
-        return instance.run().then(() => {
-            expect(summaryStub.calledThrice).to.be.true;
-            expect(tableStub.calledOnce).to.be.true;
-            expect(tableStub.args[0][0]).to.deep
-                .equal(['Name', 'Location', 'Version', 'Status', 'URL', 'Port', 'Process Manager']);
-            const rows = tableStub.args[0][1];
-            expect(rows).to.be.an.instanceof(Array);
-            expect(rows).to.have.length(3);
+        await instance.run();
+        expect(summaryStub.calledThrice).to.be.true;
+        expect(tableStub.calledOnce).to.be.true;
+        expect(tableStub.args[0][0]).to.deep
+            .equal(['Name', 'Location', 'Version', 'Status', 'URL', 'Port', 'Process Manager']);
+        const rows = tableStub.args[0][1];
+        expect(rows).to.be.an.instanceof(Array);
+        expect(rows).to.have.length(3);
 
-            const expected = [
-                ['testa', '/var/www/testa', '1.5.0', 'stopped', 'n/a', 'n/a', 'n/a'],
-                ['testb', '/var/www/testb', '1.2.0', 'running (production)', 'https://testa.com', 2369, 'systemd'],
-                ['testc', '/var/www/testc', '1.3.0', 'running (development)', 'http://localhost:2370', 2370, 'local']
-            ];
+        const expected = [
+            ['testa', '/var/www/testa', '1.5.0', 'stopped', 'n/a', 'n/a', 'n/a'],
+            ['testb', '/var/www/testb', '1.2.0', 'running (production)', 'https://testa.com', 2369, 'systemd'],
+            ['testc', '/var/www/testc', '1.3.0', 'running (development)', 'http://localhost:2370', 2370, 'local']
+        ];
 
-            expected.forEach((row, i) => {
-                row.forEach((prop, j) => {
-                    expect(stripAnsi(rows[i][j])).to.equal(prop);
-                });
+        expected.forEach((row, i) => {
+            row.forEach((prop, j) => {
+                expect(stripAnsi(rows[i][j])).to.equal(prop);
             });
         });
     });
 
-    it('Doesn\'t create a table when no instances exist', function () {
+    it('Doesn\'t create a table when no instances exist', async function () {
         const getAllInstancesStub = sinon.stub().resolves([]);
         const tableStub = sinon.stub();
         const logStub = sinon.stub();
 
         const instance = new LsCommand({log: logStub, table: tableStub}, {getAllInstances: getAllInstancesStub});
-        instance.run().then(() => {
-            expect(tableStub.called).to.be.false;
-            expect(logStub.calledOnce).to.be.true;
-            expect(logStub.args[0][0]).to.equal('No installed ghost instances found');
-        });
+        await instance.run();
+        expect(tableStub.called).to.be.false;
+        expect(logStub.calledOnce).to.be.true;
+        expect(logStub.args[0][0]).to.equal('No installed ghost instances found');
     });
 });
