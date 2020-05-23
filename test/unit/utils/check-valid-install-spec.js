f2f2370745351769e2fb20859082af502fea77d0
Vikas Potluri <vikaspotluri123.github@gmail.com>
2020-05-22 19:46:05 -0500

    feat(instance): traverse up directory tree to find instance (#820)

--- test/unit/utils/check-valid-install-spec.js
+++ test/unit/utils/check-valid-install-spec.js
@@ -1,4 +1,4 @@
-'use strict';
+// @ts-check
 
 const expect = require('chai').expect;
 const sinon = require('sinon');
@@ -11,27 +11,20 @@ describe('Unit: Utils > checkValidInstall', function () {
         sinon.restore();
     });
 
-    it('throws error if config.js present', function () {
+    it('fails when config.js present', function () {
         const existsStub = sinon.stub(fs, 'existsSync');
         existsStub.withArgs(sinon.match(/config\.js/)).returns(true);
 
-        const exitStub = sinon.stub(process, 'exit').throws();
         const errorStub = sinon.stub(console, 'error');
 
-        try {
-            checkValidInstall('test');
-            throw new Error('should not be thrown');
-        } catch (e) {
-            expect(e.message).to.not.equal('should not be thrown');
-            expect(existsStub.calledOnce).to.be.true;
-            expect(errorStub.calledOnce).to.be.true;
-            expect(exitStub.calledOnce).to.be.true;
-            expect(existsStub.args[0][0]).to.match(/config\.js/);
-            expect(errorStub.args[0][0]).to.match(/Ghost-CLI only works with Ghost versions >= 1\.0\.0/);
-        }
+        expect(checkValidInstall('test')).to.equal(false);
+        expect(existsStub.calledOnce).to.be.true;
+        expect(errorStub.calledOnce).to.be.true;
+        expect(existsStub.args[0][0]).to.match(/config\.js/);
+        expect(errorStub.args[0][0]).to.match(/Ghost-CLI only works with Ghost versions >= 1\.0\.0/);
     });
 
-    it('throws error if within a Ghost git clone', function () {
+    it('fails within a Ghost git clone', function () {
         const existsStub = sinon.stub(fs, 'existsSync');
         const readJsonStub = sinon.stub(fs, 'readJsonSync');
 
@@ -40,58 +33,38 @@ describe('Unit: Utils > checkValidInstall', function () {
         existsStub.withArgs(sinon.match(/Gruntfile\.js/)).returns(true);
         readJsonStub.returns({name: 'ghost'});
 
-        const exitStub = sinon.stub(process, 'exit').throws();
         const errorStub = sinon.stub(console, 'error');
 
-        try {
-            checkValidInstall('test');
-            throw new Error('should not be thrown');
-        } catch (e) {
-            expect(e.message).to.not.equal('should not be thrown');
-            expect(existsStub.calledThrice).to.be.true;
-            expect(errorStub.calledOnce).to.be.true;
-            expect(exitStub.calledOnce).to.be.true;
-            expect(existsStub.args[1][0]).to.match(/package\.json/);
-            expect(errorStub.args[0][0]).to.match(/Ghost-CLI commands do not work inside of a git clone/);
-        }
+        expect(checkValidInstall('test')).to.equal(false);
+        expect(existsStub.calledThrice).to.be.true;
+        expect(errorStub.calledOnce).to.be.true;
+        expect(existsStub.args[1][0]).to.match(/package\.json/);
+        expect(errorStub.args[0][0]).to.match(/Ghost-CLI commands do not work inside of a git clone/);
     });
 
-    it('throws error if above two conditions don\'t exit and .ghost-cli file is missing', function () {
+    it('neither passes nor fails when .ghost-cli file is missing', function () {
         const existsStub = sinon.stub(fs, 'existsSync');
 
         existsStub.withArgs(sinon.match(/config\.js/)).returns(false);
         existsStub.withArgs(sinon.match(/package\.json/)).returns(false);
         existsStub.withArgs(sinon.match(/\.ghost-cli/)).returns(false);
 
-        const exitStub = sinon.stub(process, 'exit').throws();
-        const errorStub = sinon.stub(console, 'error');
-
-        try {
-            checkValidInstall('test');
-            throw new Error('should not be thrown');
-        } catch (e) {
-            expect(e.message).to.not.equal('should not be thrown');
-            expect(existsStub.calledThrice).to.be.true;
-            expect(errorStub.calledOnce).to.be.true;
-            expect(exitStub.calledOnce).to.be.true;
-            expect(existsStub.args[2][0]).to.match(/\.ghost-cli/);
-            expect(errorStub.args[0][0]).to.match(/Working directory is not a recognisable Ghost installation/);
-        }
+        expect(checkValidInstall('test')).to.equal(null);
+        expect(existsStub.calledThrice).to.be.true;
+        expect(existsStub.args[2][0]).to.match(/\.ghost-cli/);
     });
 
-    it('doesn\'t do anything if all conditions return false', function () {
+    it('passes in "valid" installation', function () {
         const existsStub = sinon.stub(fs, 'existsSync');
 
         existsStub.withArgs(sinon.match(/config\.js/)).returns(false);
         existsStub.withArgs(sinon.match(/package\.json/)).returns(false);
         existsStub.withArgs(sinon.match(/\.ghost-cli/)).returns(true);
 
-        const exitStub = sinon.stub(process, 'exit').throws();
         const errorStub = sinon.stub(console, 'error');
 
-        checkValidInstall('test');
+        expect(checkValidInstall('test')).to.equal(true);
         expect(existsStub.calledThrice).to.be.true;
         expect(errorStub.called).to.be.false;
-        expect(exitStub.called).to.be.false;
     });
 });
